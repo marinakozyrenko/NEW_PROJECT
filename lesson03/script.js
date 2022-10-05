752cc73effa8adc525a6073c166ed0343c04b49d
marinakozyrenko <101600149+marinakozyrenko@users.noreply.github.com>
2022-10-06 01:01:07 +0300

    Lesson05 (#6)
    * test
    
    * home work 5

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,9 +1,15 @@
-const URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses"
-const GOODS = "/catalogData.json"
+/* const goods = [
+    {title: 'Shirt', price: 150},
+    {title: 'Socks', price: 50},
+    {title: 'Jacket', price: 350},
+    {title: 'Shoes', price: 250},
+];
+*/ 
 
-const url = `${URL}${GOODS}`;
+const BASE_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
+const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
+const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
 
-/**функция обращения на сервер за данными*/
 function service(url) {
     return new Promise((resolve) => {
         const xhr = new XMLHttpRequest();
@@ -16,14 +22,50 @@ function service(url) {
     })
 } 
 
-const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
+function init() {
+    const app = new Vue({
+        el: '#root',
+        data: {
+            items: [],
+            search: '',
+            filterItems: [],
+            isVisibleCart: false
+        },
+        methods: {
+            fetchGoods() {
+                setTimeout(() => {
+                    service(GET_GOODS_ITEMS).then((data) => {
+                        this.items = data;
+                        this.filteredItems = data;
+                    });
+                }, 2000)
+            },
+            setVisibleCard() {
+                this.isVisibleCart = !this.isVisibleCart;
+            }
+        },
+        computed: {
+            calculatePrice() {
+                return this.filteredItems.reduce((prev, { price }) => {
+                    return prev + price;
+                }, 0)
+            },
+            filteredItems() {
+                return this.items.filter((item) => {
+                    const regExp = new RegExp(this.search);
+                    return regExp.test(item.product_name)
+                });
+            },
+        },
+        mounted() {
+            this.fetchGoods();
+        }
+    })
+}
+window.onload = init
 
-class GoodsItem {
+
+/*class GoodsItem {
     constructor ({product_name="", price=0 }) {
         this.product_name = product_name;
         this.price = price;
@@ -35,12 +77,13 @@ class GoodsItem {
     </div>`;
     } 
 }
+*/
 
-class GoodsList {
+/*class GoodsList {
     items = [];
-    /**добавили в фетч функцию service, чтобы получить данные в виде result и
-    записать их в список data*/
-    fetchGoods() {
+    /**добавили в фетч функцию service, чтобы получить данные в виде result и записать их в список data*/
+    
+    /*fetchGoods() {
         return new Promise((resolve) => {
             service(url).then((data) => {
                 this.items = data;
@@ -57,17 +100,18 @@ class GoodsList {
     render() {
     /** map - вызвает переданную функцию один раз для каждого элемента
      * массива, формируя новый массив из результатов вызова этой функции
-     * join - объединяет элементы массива в строку с указанным разделителем 
-     */
-        const goods = this.items.map(item => {
-            const goodItem = new GoodsItem(item);
-            return goodItem.render();
-        }).join('');
-        document.querySelector('.goods-list').innerHTML = goods;
+     * join - объединяет элементы массива в строку с указанным разделителем */
+    
+    /*const goods = this.items.map(item => {
+    const goodItem = new GoodsItem(item);
+    return goodItem.render();
+    }).join('');
+    document.querySelector('.goods-list').innerHTML = goods;
     }
 }
 
 const goodsList = new GoodsList();
 goodsList.fetchGoods().then(() => {
-    goodsList.render();
+goodsList.render();
 })
+*/
