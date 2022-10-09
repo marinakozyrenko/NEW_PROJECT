8767c5da0ad701c07a112ef9a7bb5640a0b33e4a
marinakozyrenko <mdudar88@gmail.com>
2022-10-09 23:12:02 +0300

    lesson 07

--- lesson03/script.js
+++ lesson03/script.js
@@ -6,11 +6,12 @@
 ];
 */ 
 
-const BASE_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
-const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
-const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
+// const BASE_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
+// const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
+//const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
 
-function service(url) {
+
+/* function service(url) {
     return new Promise((resolve) => {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', url);
@@ -20,146 +21,162 @@ function service(url) {
         };
     xhr.send();
     })
-} 
+}  */
+
+// const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const BASE_URL = 'http://localhost:8000';
+const GET_GOODS_ITEMS = `${BASE_URL}/goods.json`;
+const GET_BASKET_GOODS = `${BASE_URL}/basket_goods`;
+
+function service(url, method="GET", body) {
+  return fetch(url, {
+    headers: Object.assign({}, body ? {
+      'Content-Type': 'application/json; charset=utf-8'
+    } : {}),
+    method,
+    body: JSON.stringify(body)
+  })
+  .then((res) => res.json())
+}
 
 function init() {
-    Vue.component('search-component', {
-        model: {
-            prop: 'value',
-            event: 'input'
-        },
-        props: {
-            value: String
-        },
-        template: `
-        <input type="text" class="goods-search" :value="value" @input="$emit('input', $event.target.value)" />
-        `
-    })
-
-    Vue.component('basket', {
-        template: `
-        <div class="fixed-area">
-        <div class="basket-card">
+  
+  Vue.component('search-component', {
+    model: {
+      prop: 'value',
+      event: 'input'
+    },
+    props: {
+      value: String
+    },
+    template: `
+      <input type="text" class="goods-search" :value="value" @input="$emit('input', $event.target.value)" />
+    `
+  })
+  
+  
+  const CustomButton = Vue.component('custom-button', {
+    template: `
+      <button class="search-button" type="button" v-on:click="$emit('click')">
+         <slot></slot>
+      </button>
+    `
+  })
+  const basketGoods = Vue.component('basket-goods', {
+    data() {
+      return {
+         basketGoodsItems: []
+      }
+    },
+    
+    template: `
+      <div class="fixed-area">
+         <div class="basket-card">
             <div class="basket-card__header">
-                <h1 class="basket-card__header__title">Корзина</h1>
-                <div class="basket-card__header__delete-icon" 
-                v-on:click="$emit('closeclick')"></div>
+               <h1 class="basket-card__header__title">basket card</h1>
+               <div class="basket-card__header__delete-icon"
+                  v-on:click="$emit('closeclick')"
+               ></div>
             </div>
-            <div class="basket-card__content">Информация</div>
-        </div>
-    </div>
-        `
-    });
-
-    const CustomButton = Vue.component('custom-button', {
-        template: `
-        <button class="search-button" type="button" v-on:click="$emit('click')">
-            <slot></slot>
-        </button>
-        `
-    });
-
-    Vue.component('goods_item', {
-        props: [
-            'item'
-        ],
-        temlate: `
-            <div class="goods-item">
-                <h3>{{ item.product_name }}</h3>
-                <p>{{ item.price }}</p>
+            <div class="basket-card__content">
+               <basket-goods-item v-for="item in basketGoodsItems" :item="item" @delete="deleteBasketGood" @add="addGood"></basket-goods-item>
             </div>
-        `
-    });
-
-    const app = new Vue({
-        el: '#root',
-        data: {
-            items: [],
-            search: '',
-            filterItems: [],
-            isVisibleCart: false
-        },
-        methods: {
-            fetchGoods() {
-                setTimeout(() => {
-                    service(GET_GOODS_ITEMS).then((data) => {
-                        this.items = data;
-                        this.filteredItems = data;
-                    });
-                })
-            },
-            setVisibleCard() {
-                this.isVisibleCart = !this.isVisibleCart;
-            }
-        },
-        computed: {
-            calculatePrice() {
-                return this.filteredItems.reduce((prev, { price }) => {
-                    return prev + price;
-                }, 0)
-            },
-            filteredItems() {
-                return this.items.filter((item) => {
-                    const regExp = new RegExp(this.search);
-                    return regExp.test(item.product_name)
-                });
-            },
-        },
-        mounted() {
-            this.fetchGoods();
-        }
-    })
+         </div>
+      </div>
+    `,
+    mounted() {
+      service(GET_BASKET_GOODS).then((data) => {
+        this.basketGoodsItems = data
+      })
+    },
+    methods: {
+      deleteBasketGood(id) {
+        service(GET_BASKET_GOODS, "DELETE", {
+          id
+        }).then((data) => {
+          this.basketGoodsItems = data
+        })
+      },
+      addGood(id) {
+        service(GET_BASKET_GOODS, 'PUT', {
+          id
+        }).then((data) => {
+          this.basketGoodsItems = data
+        })
+      }
+    }
+  })
+  
+  Vue.component('basket-goods-item', {
+    props: [
+      'item'
+    ],
+    template: `
+      <div class="basket-card__content___item">
+         <h3>{{item?.data?.product_name}}</h3>
+         <div>count: {{item?.count}}</div>
+         <div>total: {{item?.total}}</div>
+         <custom-button @click="$emit('add', item.data.id)">добавить</custom-button>
+         <custom-button @click="$emit('delete', item.data.id)">удалить</custom-button>
+      </div>
+    `
+  })
+  
+  const goodsItem = Vue.component('goods-item', {
+    props: [
+       'item'
+    ],
+    template: `
+      <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+         <custom-button @click="addGood">добавить</custom-button>
+      </div>
+    `,
+    methods: {
+      addGood() {
+        service(GET_BASKET_GOODS, 'PUT', {
+          id: this.item.id
+        })
+      }
+    }
+  })
+  
+  const app = new Vue({
+    el: '#root',
+    data: {
+      items: [],
+      search: '',
+      cardIsVision: false
+    },
+    methods: {
+      setVisionCard() {
+        this.cardIsVision = !this.cardIsVision
+      },
+      fetchGoods() {
+        service(GET_GOODS_ITEMS).then((data) => {
+          this.items = data;
+        });
+      },
+      onSearchComponentChange(value) {
+        this.search = value
+      }
+    },
+    computed: {
+      filteredItems() {
+        return this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
+      },
+      calculatePrice() {
+        return this.items.reduce((prev, { price }) => {
+          return prev + price;
+        }, 0)
+      }
+    },
+    mounted() {
+      this.fetchGoods();
+    }
+  })
 }
 window.onload = init
-
-
-/*class GoodsItem {
-    constructor ({product_name="", price=0 }) {
-        this.product_name = product_name;
-        this.price = price;
-    }
-    render() {
-    return `<div class="goods-item">
-    <h3>${this.product_name}</h3>
-    <p>${this.price}</p>
-    </div>`;
-    } 
-}
-*/
-
-/*class GoodsList {
-    items = [];
-    /**добавили в фетч функцию service, чтобы получить данные в виде result и записать их в список data*/
-    
-    /*fetchGoods() {
-        return new Promise((resolve) => {
-            service(url).then((data) => {
-                this.items = data;
-                resolve();
-        });
-    })
-    }
-
-    calculatePrice() {
-        return this.item.reduce((prev, item) => {
-            return prev + item.price;
-        }, 0)
-    }
-    render() {
-    /** map - вызвает переданную функцию один раз для каждого элемента
-     * массива, формируя новый массив из результатов вызова этой функции
-     * join - объединяет элементы массива в строку с указанным разделителем */
-    
-    /*const goods = this.items.map(item => {
-    const goodItem = new GoodsItem(item);
-    return goodItem.render();
-    }).join('');
-    document.querySelector('.goods-list').innerHTML = goods;
-    }
-}
-
-const goodsList = new GoodsList();
-goodsList.fetchGoods().then(() => {
-goodsList.render();
-})
-*/
